fd9fd64a659014179c0e095847aed1ef40c8670c
fzaninotto <fzaninotto@gmail.com>
2020-05-20 08:56:53 +0200

    improvements to admin ui

---
+++ precache-manifest.c1b7c2d465fd8f27e350d7e5e73bb7ad.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48aff128fff2c37b1ad2ada3f3aa76e9",
+    "url": "/react-admin-demo/index.html"
+  },
+  {
+    "revision": "b7987c69a172231f2f50",
+    "url": "/react-admin-demo/static/css/main.983be927.chunk.css"
+  },
+  {
+    "revision": "d4bd0a346a76c89186d4",
+    "url": "/react-admin-demo/static/js/0.f3b4c8ee.chunk.js"
+  },
+  {
+    "revision": "d985de5d55e0e6dbadee53aa909f70c1",
+    "url": "/react-admin-demo/static/js/0.f3b4c8ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7816e30a169805c66740",
+    "url": "/react-admin-demo/static/js/10.accccc05.chunk.js"
+  },
+  {
+    "revision": "8a2e28da19a17455a33315d1415b0e4d",
+    "url": "/react-admin-demo/static/js/10.accccc05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee00a07428799b02f90",
+    "url": "/react-admin-demo/static/js/11.4afe4543.chunk.js"
+  },
+  {
+    "revision": "79833d74db9c70bc432a2bc82a7445f3",
+    "url": "/react-admin-demo/static/js/11.4afe4543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16684e9cbca411670280",
+    "url": "/react-admin-demo/static/js/3.6c992ab6.chunk.js"
+  },
+  {
+    "revision": "55a56916b5259cc7faa7189324368078",
+    "url": "/react-admin-demo/static/js/3.6c992ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430f253a6aa8ff33a841",
+    "url": "/react-admin-demo/static/js/4.6e0ab2dd.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/react-admin-demo/static/js/4.6e0ab2dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acba415c8a022cee51bb",
+    "url": "/react-admin-demo/static/js/5.e3bc6ca3.chunk.js"
+  },
+  {
+    "revision": "9737c4626ce07a161d17",
+    "url": "/react-admin-demo/static/js/6.cc7a686e.chunk.js"
+  },
+  {
+    "revision": "ff9e7b936944cf423408",
+    "url": "/react-admin-demo/static/js/7.307a91e8.chunk.js"
+  },
+  {
+    "revision": "661c767e17b76aecef76",
+    "url": "/react-admin-demo/static/js/8.b562ee58.chunk.js"
+  },
+  {
+    "revision": "e1c98b090d0831c93a2f",
+    "url": "/react-admin-demo/static/js/9.ada4ba2a.chunk.js"
+  },
+  {
+    "revision": "b7987c69a172231f2f50",
+    "url": "/react-admin-demo/static/js/main.c4b94eb4.chunk.js"
+  },
+  {
+    "revision": "62e430169ba3c4c35168",
+    "url": "/react-admin-demo/static/js/runtime-main.a68bffc0.js"
+  }
+]);
